ae6d5b367f88272672cc8329ce2b7075cc79ef8d
yousuf-mousani <khananirafiq@gmail.com>
2023-11-02 21:58:38 +0500

    Authentication

---
+++ app2.js
@@ -0,0 +1,37 @@
+import {signInWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/9.10.0/firebase-auth.js";
+import { auth } from "./config.js";
+
+const form = document.querySelector('#form');
+const email = document.querySelector('#email');
+const password = document.querySelector('#password');
+
+
+form.addEventListener('submit', (event) => {
+    event.preventDefault();
+    signInWithEmailAndPassword(auth, email.value, password.value)
+        .then((userCredential) => {
+            const user = userCredential.user;
+            console.log(user);
+            window.location = 'home.html'
+        })
+        .catch((error) => {
+            const errorCode = error.code;
+            const errorMessage = error.message;
+            console.log(errorMessage);
+        });
+
+})
+
+
+
+
+// forgotBtn.addEventListener('click' , ()=>{
+//     const user = auth.currentUser;
+//     const newPassword = prompt('enter new password');
+//     updatePassword(user, newPassword).then(() => {
+//         // Update successful.
+//       }).catch((error) => {
+//         // An error ocurred
+//         // ...
+//       });
+// })
